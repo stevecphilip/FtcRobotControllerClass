1067c8987005978b2fc1cce165e4db33270a8e95
ps12072448 <philips2@go.lisd.net>
2025-09-03 13:56:03 -0500

    Stove

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTouch.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTouch.java
@@ -1,63 +1,27 @@
 package org.firstinspires.ftc.teamcode;
 
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.hardware.Gamepad;
-
-import java.lang.annotation.ElementType;
 
 @TeleOp
-public class touchServo extends LinearOpMode {
-    private Servo servo;
-    private TouchSensor touchsensor;
-    private boolean flop = false;
-
+public class ServoAndTouch extends LinearOpMode {
+    private Servo testServo = null;
+    private TouchSensor touchSensor = null;
 
     @Override
-    public void runOpMode() throws InterruptedException {
-        double runtime = getRuntime();
-        servo = hardwareMap.get(Servo.class, "test_servo");
-        touchsensor = hardwareMap.get(TouchSensor.class, "touch_sensor");
-        ElapsedTime elapsedTime = new ElapsedTime();
+    public void runOpMode() {
+        testServo = hardwareMap.get(Servo.class, "test_servo");
+        touchSensor = hardwareMap.get(TouchSensor.class, "touch_sensor");
 
-        double time = elapsedTime.time();
+        telemetry.addLine("Initialized -Press Start");
+        telemetry.update();
 
         waitForStart();
-
-        while (opModeIsActive()) {
-            if (touchsensor.isPressed()) {
-                if (flop) {
-                    servo.setPosition(0);
-                    double current = elapsedTime.time();
-                    while (elapsedTime.time() - current < 1) {
-
-                    }
-                    flop = false;
-                } else {
-                    servo.setPosition(1.0);
-                    double current = elapsedTime.time();
-                    while (elapsedTime.time() - current < 1) {
-
-                    }
-                    flop = true;
-                }
-
-            } else {
-                servo.setPosition(servo.getPosition());
-            }
-
-
-            telemetry.addData("Runtime", runtime);
-            telemetry.addData("Servo Position", servo.getPosition());
-            telemetry.addData("Touch Sensor Value", touchsensor.isPressed());
-            telemetry.update();
-        }
-
-
+        ElapsedTime timer = new ElapsedTime()
+                boolean forward = true;
     }
 }
-
-
