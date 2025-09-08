11329264042f26b8608c824f323f30fb8c4f6189
ps12072448 <philips2@go.lisd.net>
2025-09-08 13:54:03 -0500

    Stove

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorPractice.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+@TeleOp (name = "ColorSensorPractice", group = "Program")
+public class ColorSensorPractice extends LinearOpMode {
+    private ColorSensor color;
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorTest.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.mechanisms.TestBench;
+
+
+public class ColorSensorTest extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
