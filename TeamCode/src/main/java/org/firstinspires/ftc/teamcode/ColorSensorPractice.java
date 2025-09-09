bcca29cc4e03c91a24c8aef6dd88df9c4681886b
ps12072448 <philips2@go.lisd.net>
2025-09-09 13:29:08 -0500

    Stove

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorPractice.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -10,17 +9,21 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.JavaUtil;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.mechanisms.TestBench;
 
 @TeleOp (name = "ColorSensorPractice", group = "Program")
 public class ColorSensorPractice extends LinearOpMode {
     private ColorSensor color;
     private DistanceSensor distanceSensor;
     ElapsedTime elapsedTime = new ElapsedTime();
+    TestBench bench = new TestBench();
 
     @Override
     public void runOpMode(){
+        bench.init(hardwareMap);
+
         ColorSensor color = hardwareMap.get(ColorSensor.class, "color_sensor");
-        DistanceSensor distance = hardwareMap.get(DistanceSensor.class, "sensor_distance");
+        DistanceSensor distance = hardwareMap.get(DistanceSensor.class, "color_sensor");
         waitForStart();
 
         while (opModeIsActive()) {
