05b0fc027357ea8ad7cb96183cbe98a741bb3393
ps12072448 <philips2@go.lisd.net>
2025-09-10 13:55:01 -0500

    Stove

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorPractice.java
@@ -12,6 +12,7 @@ public class ColorSensorPractice extends LinearOpMode {
     private ColorSensor color;
     private DistanceSensor distanceSensor;
     ElapsedTime elapsedTime = new ElapsedTime();
+    float[] hsv = new float[3];
 
 
     @Override
@@ -19,19 +20,20 @@ public class ColorSensorPractice extends LinearOpMode {
         DcMotor dcMotor = hardwareMap.get(DcMotor.class, "test_motor");
         ColorSensor color = hardwareMap.get(ColorSensor.class, "color_sensor");
         DistanceSensor distance = hardwareMap.get(DistanceSensor.class, "color_sensor");
-        float[] hsv = new float[3];
         waitForStart();
-        int red = color.red();
-        int green = color.green();
-        int blue = color.blue();
-        android.graphics.Color.RGBToHSV(red, green, blue, hsv);
-        float hue = hsv[0];
-        float sat = hsv[1];
-        float val = hsv[2];
+
         while (opModeIsActive()) {
-            telemetry.addData("Red", red);
-            telemetry.addData("Green", green);
-            telemetry.addData("Blue", blue);
+            int red = color.red();
+            int green = color.green();
+            int blue = color.blue();
+            android.graphics.Color.RGBToHSV(red, green, blue, hsv);
+            float hue = hsv[0];
+            float sat = hsv[1];
+            float val = hsv[2];
+
+            telemetry.addData("Red", color.red());
+            telemetry.addData("Green", color.green());
+            telemetry.addData("Blue", color.blue());
             telemetry.addData("Hue", hue);
             telemetry.addData("Saturation", sat);
             telemetry.addData("Value", val);
