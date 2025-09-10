94da9cf6a97cf126a46b5dc59ed88f7c1cd9fb32
ps12072448 <philips2@go.lisd.net>
2025-09-10 12:58:25 -0500

    Stove

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorPractice.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -9,19 +10,15 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.JavaUtil;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.teamcode.mechanisms.TestBench;
 
 @TeleOp (name = "ColorSensorPractice", group = "Program")
 public class ColorSensorPractice extends LinearOpMode {
     private ColorSensor color;
     private DistanceSensor distanceSensor;
     ElapsedTime elapsedTime = new ElapsedTime();
-    TestBench bench = new TestBench();
 
     @Override
     public void runOpMode(){
-        bench.init(hardwareMap);
-
         ColorSensor color = hardwareMap.get(ColorSensor.class, "color_sensor");
         DistanceSensor distance = hardwareMap.get(DistanceSensor.class, "color_sensor");
         waitForStart();
@@ -32,7 +29,7 @@ public class ColorSensorPractice extends LinearOpMode {
             telemetry.addData("Blue", color.blue());
             telemetry.addData("Hue", (JavaUtil.colorToHue(color.blue())));
             telemetry.addData("Run Time", elapsedTime.time());
-            telemetry.addData("Distance", distance.getDistance(DistanceUnit.CM));
+            telemetry.addData("Distance", distance.getDistance(DistanceUnit.MM));
             telemetry.update();
         }
     }
